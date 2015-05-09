7d57cc37d065a50934ff64868e3c0ebe389903c7
Martin Ringwelski <martin@martimedia.de>
2015-05-09 16:10:01 +0200

    - background music works

--- utopia/SoundThread.cpp
+++ utopia/SoundThread.cpp
@@ -6,6 +6,10 @@
  */
 
 #include "SoundThread.h"
+#include <iterator>
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
+using namespace boost::filesystem;
 
 SoundThread* SoundThread::startThread() {
 	SoundThread* ret = new SoundThread();
@@ -20,15 +24,76 @@ SoundThread* SoundThread::startThread() {
 }
 
 SoundThread::SoundThread():
-		thread(NULL)
+		thread(NULL),
+		quit(false),
+		music(NULL)
 {
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&condition, NULL);
 }
 
 SoundThread::~SoundThread() {
+	pthread_mutex_lock(&mutex);
+	quit = true;
+	pthread_mutex_unlock(&mutex);
+	pthread_cond_signal(&condition);
 	SDL_WaitThread(thread, NULL);
+	if (music != NULL) {
+		Mix_FreeMusic( music );
+	}
 }
 
 int SoundThread::threadMethod(void* param) {
 	SoundThread* t = (SoundThread*)param;
+
+	if (Mix_OpenAudio( 22050, MIX_DEFAULT_FORMAT, 2, 4096 ) == -1) {
+		return 0;
+	}
+	path p ("./sound/");
+
+	try
+	{
+		if (exists(p)) {
+			if (is_directory(p)) {
+				directory_iterator it(p), eod;
+				BOOST_FOREACH(path const &f, std::make_pair(it, eod)) {
+				    if(is_regular_file(f))
+				    {
+				    	if (f.string().find(".mp3") != std::string::npos) {
+				    		t->musicFiles.push_back(f.string());
+				    	}
+				    }
+				}
+			}
+		} else {
+			Mix_CloseAudio();
+			return 0;
+		}
+	} catch (const filesystem_error& ex) {
+		Mix_CloseAudio();
+		return 0;
+	}
+
+	pthread_mutex_lock(&(t->mutex));
+	while (!t->quit) {
+		pthread_mutex_unlock(&(t->mutex));
+		if( Mix_PlayingMusic() == 0 ) {
+			if (t->music != NULL) {
+				Mix_FreeMusic( t->music );
+			}
+			int numFile = rand() % t->musicFiles.size();
+			t->music = Mix_LoadMUS( t->musicFiles[numFile].c_str() );
+
+			if( Mix_PlayMusic( t->music, 1 ) == -1 ) {
+				return 0;
+			}
+		}
+		//pthread_cond_wait(&(t->condition), &(t->mutex));
+		pthread_mutex_lock(&(t->mutex));
+	}
+	pthread_mutex_unlock(&(t->mutex));
+
+	Mix_HaltMusic();
+	Mix_CloseAudio();
 	return 0;
 }
--- utopia/SoundThread.h
+++ utopia/SoundThread.h
@@ -9,17 +9,31 @@
 #define SOUND_SOUNDTHREAD_H_
 
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_mixer.h"
+#include <pthread.h>
+#include <vector>
+#include <string>
 
 class SoundThread {
 public:
 	static SoundThread* startThread();
+
 	~SoundThread();
+	void quitThread() {
+		quit = true;
+	}
 private:
 	SoundThread();
 
 	static int threadMethod(void* param);
 
 	SDL_Thread* thread;
+	pthread_mutex_t mutex;
+	pthread_cond_t condition;
+
+	bool quit;
+	std::vector<std::string>	musicFiles;
+	Mix_Music *music;
 
 };
 
